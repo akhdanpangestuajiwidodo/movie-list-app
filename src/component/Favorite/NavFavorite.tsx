d03a75fa75c45a6b6aeed82c57b8aba0e9ae08c9
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-02-24 13:45:59 +0700

    link to back

--- src/component/Favorite/NavFavorite.tsx
+++ src/component/Favorite/NavFavorite.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./navfavorite.css";
+import { Link } from "react-router-dom";
 
 const NavFavorite = () => {
   return (
     <div className="navfavorite-parent">
       <ul>
-        <li className="navfavorite-left">
-          <a href="default.asp">Kembali</a>
-        </li>
+        <Link to={"/"}>
+          <li className="navfavorite-left">
+            <a href="default.asp">Kembali</a>
+          </li>
+        </Link>
+
         <div className="navfavorite-right">
           <li className="">
             <a href="news.asp">Favorite Movie</a>
