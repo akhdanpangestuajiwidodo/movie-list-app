f90056e808cefa1693baa54c035c0ece75939014
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-02-25 10:14:34 +0700

    create get ongoing api

--- src/service/ApiServices.tsx
+++ src/service/ApiServices.tsx
@@ -1,1 +1,28 @@
 import axios from "axios";
+
+//Base Url Api
+const baseUrl = "https://api.themoviedb.org/3/";
+
+//Get API (Ongoing Movie)
+const getOngoingMovie = (page: any) => {
+  return axios({
+    method: "get",
+    url:
+      baseUrl +
+      "/movie/now_playing?api_key=be7ddc7074fe58edbe5eb7645a53072d&language=en-US&page=" +
+      page,
+  })
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+};
+
+export { getOngoingMovie };
